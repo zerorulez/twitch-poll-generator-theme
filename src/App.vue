28b101567c9e66e5987a6bde74a8465e81deaff4
Lucas Ribeiro <zero@bbl.gg>
2021-04-09 13:47:36 -0300

    animacoes simples

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div id="app" v-if="results.length > 0">
-    <h1>Replys</h1>
+  <div id="app" :class="{'visible': results.length > 0}">
+    <h2>Replys</h2>
     <div class="reply" v-for="(item, index) in results" :key="index">
       <div class="reply-header">
         <p>{{item.word}}</p>
         <p>{{item.count}} Votes</p>
       </div>
       <div class="reply-body">
-        <div class="result-bar" :style="{width: item.percentage + '%' }"></div>
+        <div class="result-bar-bg">
+          <div class="result-bar" :style="{width: item.percentage + '%' }"></div>
+        </div>
         <div class="percentage">
           <p>{{Math.trunc(item.percentage)}}%</p>
         </div>
@@ -27,7 +29,6 @@ export default {
   sockets: {
     results: function(data) {
       this.results = data
-      console.log(this.results)
     }
   },
 }
@@ -42,10 +43,29 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  opacity: 0;
+  transition: opacity .3s;
+}
+.visible {
+  opacity: 1 !important;
+}
+p {
+  font-weight: bold;
 }
 .result-bar{
   min-height: 45px;
-  background-color: yellowgreen;
+  transition: width .3s;
+}
+.result-bar:nth-child(even) {
+  background-color: #52734d;
+}
+.result-bar:nth-child(odd) {
+  background-color: #91c788;
+}
+.result-bar-bg {
+  width: 100%;
+  background-color: lightgray;
+  height: 100%;
 }
 .reply-header {
   display: flex;
